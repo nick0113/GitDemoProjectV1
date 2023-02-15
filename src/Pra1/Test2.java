18a21d361f53b2e7d37c5a6d519b12f0e547a307
Admin <Admin@LAPTOP-RBDFPJS2>
2023-02-15 16:04:09 +0530

    Hiiii

---
+++ src/Pra1/Test2.java
@@ -0,0 +1,28 @@
+package Pra1;
+
+public class Test2 {
+
+	
+	public static void main(String[] args) {
+		
+		int a = 20 ;
+		int b = 356;
+		int c = 245;
+		int d = 458;
+		
+		if (a<b)
+		{
+			System.out.println("Nikhil");
+		}
+		else
+		{
+			System.out.println("Pratik");
+		}
+	}
+
+}
+public class Mohini {
+	System.out.prinln("Hello");
+}
+
+
