08053b2df63ecfc6fd8410eeb6286e10024ede4c
Admin <Admin@LAPTOP-RBDFPJS2>
2023-02-15 16:28:00 +0530

    Mohini class

---
+++ src/Pra1/Mohini.java
@@ -0,0 +1,35 @@
+package Pra1;
+
+public class Mohini {
+
+	
+		public static void main(String[] args) {
+			
+			int a = 20 ;
+			int b = 356;
+			int c = 245;
+			int d = 458;
+			
+			if (a<b)
+			{
+				System.out.println("Nikhil");
+				System.out.println("Pranita");
+				System.out.println("Jayesh");
+			}
+			else
+			{
+				System.out.println("Pratik");
+				System.out.println("Samiksha");
+				System.out.println("Anirudh");
+			}
+		}
+
+	}
+	public class Mohini {
+		System.out.prinln("Hello");
+	}
+
+
+	}
+
+}
