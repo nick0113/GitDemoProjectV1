9102c943c4dc641eb7257f54dad2ce31e7a8c673
ADMIN <ADMIN@DESKTOP-U2QV9TM>
2023-02-15 10:08:27 +0530

    First Commit

---
+++ src/Pra1/Test1.java
@@ -0,0 +1,25 @@
+package Pra1;
+
+public class Test1 {
+
+	
+	public static void main(String[] args) {
+		
+		int a = 20 ;
+		int b = 356;
+		int c = 245;
+		int d = 458;
+		
+		if (a<b)
+		{
+			System.out.println("Nikhil");
+		}
+		else
+		{
+			System.out.println("Pratik");
+		}
+	}
+
+}
+
+
