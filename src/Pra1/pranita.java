f5e86e971a4dc3444f3571059ec5a9323142430c
Dell <Dell@DESKTOP-D862IQ3>
2023-02-16 16:13:47 +0530

    new class created name- pranita

---
+++ src/Pra1/pranita.java
@@ -0,0 +1,17 @@
+package Pra1;
+
+public class pranita {
+
+	
+	public static void main(String[] args) {
+		
+		int a = 20 ;
+		int b = 356;
+		int c = 245;
+		int d = 458;
+		
+		
+		System.out.println("pranita");
+	}
+
+}
